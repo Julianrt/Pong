0d0e1795fb09b4010f9399523089007c72192773
Julian Ruiz <jnrt96@gmail.com>
2016-08-22 00:41:23 -0700

    creé la clase raqueta, y agregue 2 raquetas al tablero, tambien tienen movimiento con eventos del teclado

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,16 +6,27 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 public class Game extends JPanel{
 
     protected Ball ball;
 
+    protected Racquet playerONE;
+
+    protected Racquet playerTWO;
+
+
 
     public Game(){
+        this.setSize(600,500);
         ball = new Ball(this);
+        playerTWO = new Racquet(this,getWidth()-66,KeyEvent.VK_UP,KeyEvent.VK_DOWN);
+        playerONE = new Racquet(this,50,KeyEvent.VK_W,KeyEvent.VK_S);
         this.setFocusable(true);
         this.setBackground(Color.BLACK);
+        this.addKeyListener(playerONE.listener);
+        this.addKeyListener(playerTWO.listener);
     }
 
     @Override
@@ -25,10 +36,14 @@ public class Game extends JPanel{
         g2d.setColor(Color.WHITE);
         g2d.fillRect(getWidth()/2,0,1,getHeight());
         ball.paint(g2d);
+        playerONE.paint(g2d);
+        playerTWO.paint(g2d);
     }
 
     public void move(){
         ball.move();
+        playerONE.move();
+        playerTWO.move();
     }
 
     public static void main(String[] args) {
---
+++ src/com/company/Racquet.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.Graphics2D;
+
+public class Racquet {
+
+    private int x,y;
+
+    private int moveY;
+
+    private final int WIDTH = 16;
+
+    private final int HEIGHT = 32;
+
+    private Game app;
+
+    public KeyListener listener;
+
+    public Racquet(Game app, int x, int keyUP, int keyDOWN){
+        this.app = app;
+        this.x = x;
+        this.y = app.getHeight()/2-32;
+        this.moveY = 0;
+
+         listener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == keyUP)
+                    moveY=-1;
+                else if(e.getKeyCode() == keyDOWN)
+                    moveY=1;
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if(e.getKeyCode()==keyUP || e.getKeyCode()==keyDOWN)
+                    moveY=0;
+            }
+        };
+    }
+
+    public void paint(final Graphics2D g2d){
+        g2d.fillRect(x,y,WIDTH,HEIGHT);
+    }
+
+    public void move(){
+        if((moveY==-1 && y>=0) || (moveY==1&&y<=app.getHeight()-HEIGHT))
+            y+=moveY;
+    }
+
+
+}
